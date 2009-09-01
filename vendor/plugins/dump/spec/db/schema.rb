912b45130e13f35acbfe4b7da44a3a61e98ffe43
Andrew Daskaliuk <drongous@gmail.com>
2009-09-01 07:22:50 +0300

    Delede dump plugins

--- vendor/plugins/dump/spec/db/schema.rb
+++
@@ -1,7 +0,0 @@
-ActiveRecord::Schema.define(:version => 0) do
-  create_table :chickens, :force => true do |t|
-    %w(string text integer float decimal datetime timestamp time date binary boolean).each do |type|
-      t.column "#{type}_col", type
-    end
-  end
-end
